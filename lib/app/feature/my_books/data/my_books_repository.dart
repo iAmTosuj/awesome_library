e07b3f2ac200bc81e315620a78f320e93f3f1ee5
Tosuj <sanches00799@gmail.com>
2022-05-03 19:43:10 +0300

    feat(my_book): create my book repository

---
+++ lib/app/feature/my_books/data/my_books_repository.dart
@@ -0,0 +1,5 @@
+import 'package:library_web/app/feature/my_books/model/my_book_response.dart';
+
+abstract class MyBooksRepository {
+  Future<List<MyBookResponse>> getBooks();
+}
---
+++ lib/app/feature/my_books/data/my_books_repository_impl.dart
@@ -0,0 +1,19 @@
+import 'package:library_web/app/common/network_client/main_network_client.dart';
+import 'package:library_web/app/feature/my_books/data/my_books_repository.dart';
+import 'package:library_web/app/feature/my_books/model/my_book_response.dart';
+
+class MyBooksRepositoryImpl extends MyBooksRepository {
+  final MainNetworkClient _mainNetworkClient;
+  static const _path = '';
+
+  MyBooksRepositoryImpl({
+    required MainNetworkClient mainNetworkClient,
+  }) : _mainNetworkClient = mainNetworkClient;
+
+  @override
+  Future<List<MyBookResponse>> getBooks() async {
+    final response = await _mainNetworkClient.client.get(_path);
+
+    return response.data.map((e) => MyBookResponse.fromJson(e));
+  }
+}
