d8ba2b5fa81e6ca5fa527a2a61c19c7056e75485
Tosuj <sanches00799@gmail.com>
2022-05-12 20:59:24 +0300

    feat(main): add cookie parse

--- lib/app/common/network_client/base_network_client.dart
+++ lib/app/common/network_client/base_network_client.dart
@@ -1,6 +1,6 @@
-import 'package:cookie_jar/cookie_jar.dart';
+import 'dart:html';
+
 import 'package:dio/dio.dart';
-import 'package:dio_cookie_manager/dio_cookie_manager.dart';
 import 'package:library_web/app/common/injector/d_i.dart';
 import 'package:library_web/app/common/router/app_state.dart';
 
@@ -8,13 +8,28 @@ abstract class BaseNetworkClient {
   final Dio client;
 
   BaseNetworkClient(this.client) {
-    client.interceptors.add(CookieManager(CookieJar()));
-    client.interceptors.add(InterceptorsWrapper(onError: (DioError e, handler) {
-      if (e.response?.statusCode == 403) {
-        DI.find<AppState>().setAuthState(false);
-      }
+    client.interceptors.add(InterceptorsWrapper(
+      onRequest: (option, handler) {
+        final cookie = document.cookie!;
 
-      return handler.next(e); //continue
-    }));
+        final entity = cookie.split("; ").map((item) {
+          final split = item.split("=");
+          return MapEntry(split[0], split[1]);
+        });
+
+        final cookieMap = Map.fromEntries(entity);
+
+        option.headers['X-CSRFToken'] = cookieMap['csrftoken'];
+
+        handler.next(option);
+      },
+      onError: (DioError e, handler) {
+        if (e.response?.statusCode == 403) {
+          DI.find<AppState>().setAuthState(false);
+        }
+
+        return handler.next(e); //continue
+      },
+    ));
   }
 }
